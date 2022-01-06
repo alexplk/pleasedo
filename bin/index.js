143c88df096466502c5259269ca28b023bac47d5
Alex Poliakov <alexplk@icloud.com>
2022-01-06 01:09:09 -0500

    Command line args #1

--- bin/index.js
+++ bin/index.js
@@ -8,6 +8,46 @@ const AutocompletePrompt = require('inquirer-autocomplete-prompt');
 
 const CommandFinder = require('./CommandFinder');
 
+async function main() {
+  const args = process.argv.slice(2);
+  const commandFinder = new CommandFinder('.');
+  let command = args.length > 0 ?
+    await findNonInteractive(commandFinder, args.join(' ')) :
+    await findInteractive(commandFinder);
+  runCommand(command);
+}
+
+async function findInteractive(commandFinder) {
+  inquirer.registerPrompt('commandsearch', AutocompletePrompt)
+  const result = await inquirer.prompt([
+    {
+      type: 'commandsearch',
+      name: 'command',
+      pageSize: 15,
+      source: (_, pattern) => findCommandSource(commandFinder, pattern),
+      message: 'Command',
+      suffix: ':',
+      // prefix: '?',
+      searchText: 'Searching...',
+      emptyText: 'No results',
+      default: '',
+      suggestOnly: false,
+    }
+  ]);
+  return result.command;
+}
+
+async function findNonInteractive(commandFinder, pattern) {
+  const commandGroups = await commandFinder.find(pattern);
+  if (commandGroups && commandGroups.length > 0) {
+    const group = commandGroups[0];
+    if (group && group.commands.length > 0) {
+      return group.commands[0];
+    }
+  }
+  return null;
+}
+
 async function findCommandSource(commandFinder, pattern) {
   const commandGroups = await commandFinder.find(pattern);
   return [...commandGroups.flatMap(group => {
@@ -16,13 +56,18 @@ async function findCommandSource(commandFinder, pattern) {
       ...group.commands.map(command => ({
         name: command.match,
         value: command,
-        short: command.name,
+        short: pattern,
       }))
     ]
   }), new inquirer.Separator(' ')];
 }
 
 function runCommand(command) {
+  if (!command) {
+    console.error(style.red.open + 'No results' + style.red.close);
+    return;
+  }
+
   console.log(style.green.open + command.preview + style.green.close);
   const handleError = (error) => {
     console.error(error.message);
@@ -39,29 +84,6 @@ function runCommand(command) {
   } catch (error) {
     handleError(error);
   }
-}
-
-async function main() {
-  const commandFinder = new CommandFinder('.');
-  inquirer.registerPrompt('commandsearch', AutocompletePrompt)
-  const result = await inquirer.prompt([
-    {
-      type: 'commandsearch',
-      name: 'command',
-      pageSize: 15,
-      source: (_, pattern) => findCommandSource(commandFinder, pattern),
-      message: 'Command',
-      suffix: ':',
-      // prefix: '?',
-      searchText: 'Searching...',
-      emptyText: 'No results',
-      default: '',
-      suggestOnly: false,
-
-    }
-  ]);
-
-  runCommand(result.command);
 }
 
 main();
